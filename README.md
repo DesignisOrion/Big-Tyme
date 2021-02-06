3f819c236f2489f760c8abb4e5847235986eaddb
Orion Ford <39802627+DesignisOrion@users.noreply.github.com>
2021-02-06 16:19:47 -0500

    adding files

---
+++ README.md
@@ -0,0 +1,4 @@
+# Big Tyme (A simple timer using Python)
+
+Goal: Once timer is up, it plays the song "Rick Ross - Big Tyme ft. Swizz Beatz"
+
---
+++ timer.py
@@ -0,0 +1,19 @@
+# import the time / playsound modules 
+import time 
+
+# define the countdown function
+def countdown(t): 
+	
+	while t: 
+		mins, secs = divmod(t, 60) 
+		timer = '{:02d}:{:02d}'.format(mins, secs) 
+		print(timer, end="\r") 
+		time.sleep(1) 
+		t -= 1
+	
+	print('TIMES UP! Take a break Orion.')
+
+# input time in seconds 
+t = input("Enter the time in seconds: ")
+# function call 
+countdown(int(t)) 
